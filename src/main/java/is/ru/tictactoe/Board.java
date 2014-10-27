d2064bbdfca5289ba063c4c86a178f9bc1755610
Sonja Jonsdottir <sonja13@i-74a55db6.greenqloud.internal>
2014-10-27 19:13:12 +0000

    Created a global variable for the board and edited for loop

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -1,14 +1,16 @@
 public class Board{
+	private static char[] board;
 	//Constructor
         public Board(){
                 //Create an array of size 9 that represents board
-                char[] board = new char[9];
+                board = new char[9];
                 //Initialize board with the number of the field
 		/* 1 2 3
 		*  4 5 6
 		*  7 8 9 */ 
-                for(int i = 1; i < 10; i++){
-                        board[i] = (char) i;
+                for(int i = 0; i < 9; i++){
+			int num = i+1;
+                        board[i] = (char) num;
                 }
         }
 }
