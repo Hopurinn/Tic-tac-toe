fe7b6a45313f75e254e15f637784f7d80b14734a
Sonja Jonsdottir <sonja13@ru.is>
2014-10-28 12:56:54 +0000

    Edited drawboard, both test and main, added a column separator

--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -12,7 +12,7 @@ public class BoardTest {
 
 	@Test
 	public void testDrawBoard() {
-		assertEquals(" 1 2 3 \n 4 5 6 \n 7 8 9 ", board.drawBoard());
+		assertEquals(" | 1 | 2 | 3 | \n | 4 | 5 | 6 | \n | 7 | 8 | 9 | ", board.drawBoard());
 	}
 
 	@Test
