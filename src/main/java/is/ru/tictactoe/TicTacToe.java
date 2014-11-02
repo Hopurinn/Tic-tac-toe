be8b73165e0a46ce3d134b1166a598e57a4079b0
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2014-11-02 02:30:02 +0000

    Main program now runs web server

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,21 +1,26 @@
 package is.ru.tictactoe;
 
-import java.util.Scanner;
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
 
-public class TicTacToe {
-	// scanner to determine if the player wishes to play again
-	private static Scanner input = new Scanner(System.in);
-
-	// main
+public class TicTacToe implements SparkApplication{
 	public static void main(String[] args) {
-		char c;
-		do {
-			// runs the game
-			Game.run();
-			System.out.println("Do you want to play again?(y/n)");
-			c = input.next().charAt(0);
-		// runs until the user choses to quit (n)
-		} while(c == 'y');
-		System.out.println("BYE! Thanks for playing, looking forward to seeing you again next time");
+		staticFileLocation("/public");
+		SparkApplication tictactoe = new TicTacToe();
+		String port = System.getenv("PORT");
+        	if (port != null) {
+         	   	setPort(Integer.valueOf(port));
+        	}
+		tictactoe.init();
+	}
+
+	public void init(){
+		get(new Route("/test"){
+			@Override
+			public Object handle(Request request, Response response){
+				return "Hello";
+			}
+		});
 	}
 }
